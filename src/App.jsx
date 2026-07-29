adc5ea75ee4a97987b1660ac1d80bab73aafc4cf
AdityaNaikwad <adityanaikwad04@gmail.com>
2026-07-29 17:54:16 +0530

    added HR designation

--- src/App.jsx
+++ src/App.jsx
@@ -134,7 +134,7 @@ function App() {
           <Route
             path="/admin"
             element={
-              <ProtectedRoute allowedRoles={["admin"]}>
+              <ProtectedRoute allowedRoles={["admin", "hr"]}>
                 <AdminLayout />
               </ProtectedRoute>
             }
@@ -208,7 +208,7 @@ function App() {
           <Route
             path="/pm"
             element={
-              <ProtectedRoute allowedRoles={["pm"]}>
+              <ProtectedRoute allowedRoles={["pm", "hr"]}>
                 <EmployeeLayout />
               </ProtectedRoute>
             }
